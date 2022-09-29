753e136279aa361d4d4f19d744c098de23485b23
Rashid Shamsudinov <rash2x@gmail.com>
2022-09-29 23:45:36 +0530

    check other vars

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import {telegram} from "./telegram.js";
 import {Button} from "@mui/material";
 
 function App() {
-  const userData = telegram.initDataUnsafe.user;
+  const {user, chat} = telegram.initDataUnsafe.user;
   useEffect(() => {
     telegram.ready();
   })
 
-  console.log(userData);
+  console.log(telegram.initData);
+  console.log(user, chat);
 
   return (
     <div className="App">
-      <img src={userData.photo_url} alt="photo"/>
-      <p>Привет {userData.username}</p>
+      <img src={user.photo_url} alt="photo"/>
+      <p>Привет {user.username}</p>
       Расшифруйте особенности вашего тела, узнайте что на вас влияет, какая у вас стратегия жизни, какие камни вам
       подходят и многое другое
       <Button color="primary">Расшифровать</Button>
