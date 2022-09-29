95290d61b38673065f249ad055136ef196092020
Rashid Shamsudinov <rash2x@gmail.com>
2022-09-29 23:41:47 +0530

    add user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {telegram} from "./telegram.js";
 import {Button} from "@mui/material";
 
 function App() {
-
+  const userData = telegram.initDataUnsafe.user;
   useEffect(() => {
     telegram.ready();
   })
 
+  console.log(userData);
+
   return (
     <div className="App">
+      <img src={userData.photo_url} alt="photo"/>
+      <p>Привет {userData.username}</p>
       Расшифруйте особенности вашего тела, узнайте что на вас влияет, какая у вас стратегия жизни, какие камни вам
       подходят и многое другое
       <Button color="primary">Расшифровать</Button>
