7cf6b7d5395655d83b559d8a007093d1ccf405db
Rashid Shamsudinov <rash2x@gmail.com>
2022-09-29 23:25:15 +0530

    css-baseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {createTheme, ThemeProvider} from "@mui/material";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import {telegram} from "./telegram.js";
 
 const theme = createTheme({
@@ -23,6 +23,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App/>
     </ThemeProvider>
   </React.StrictMode>
