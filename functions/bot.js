5ccb1688468e7bf31b0399bec37a2e73d1f1a8ec
Rashid Shamsudinov <rash2x@gmail.com>
2022-09-29 22:37:27 +0530

    add telegram bot

---
+++ functions/bot.js
@@ -0,0 +1,14 @@
+const {Telegraf} = require("telegraf");
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+const web_link = "https://golden-flan-135f29.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Расшифруйте особенности вашего тела, узнайте что на вас влияет, какая у вас стратегия жизни, какие камни вам подходят и многое другое", {
+    reply_markup: {
+      keyboard: [[{text: "swami", web_app: {url: web_link}}]],
+    },
+  })
+);
+
+bot.launch();
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const telegram = window.Telegram.WebApp
+
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    telegram.ready();
+  })
 
   return (
     <div className="App">
