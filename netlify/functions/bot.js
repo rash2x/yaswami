c834a2c4fdbe42c7f22dae46b40f7d54091f0091
Rashid Shamsudinov <rash2x@gmail.com>
2022-10-30 03:05:38 +0530

    update

---
+++ netlify/functions/bot.js
@@ -0,0 +1,16 @@
+import {Telegraf} from "telegraf";
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const web_link = "https://golden-flan-135f29.netlify.app/";
+
+bot.start((ctx) => {
+  console.log(ctx);
+
+  return ctx.reply("Расшифруйте особенности вашего тела, узнайте что на вас влияет, какая у вас стратегия жизни, какие камни вам подходят и многое другое", {
+    reply_markup: {
+      keyboard: [[{text: "swami", web_app: {url: web_link}}]],
+    },
+  })
+});
+
+bot.launch();
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import {useEffect, useState} from 'react'
 import {telegram} from "./telegram.js";
-import {Button, Card, Typography} from "@mui/material";
-import Event from "./components/Event.jsx";
 import Events from "./components/Events.jsx";
 
 function App() {
-
   const [user, setUser] = useState({
     username: ''
   });
