48975d672175d511b168b0410cff924c873bdd9f
Rashid Shamsudinov <rash2x@gmail.com>
2022-10-30 03:37:38 +0530

    fixes

--- functions/bot.js
+++ functions/bot.js
@@ -1,4 +1,4 @@
-import {Telegraf} from "telegraf";
+const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const web_link = "https://golden-flan-135f29.netlify.app/";
