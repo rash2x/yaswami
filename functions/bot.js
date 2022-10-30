c6f49b0dab0ba5e44c8134be3743d3655033f566
Rashid Shamsudinov <rash2x@gmail.com>
2022-10-30 09:09:42 +0530

    check registration

--- functions/bot.js
+++ functions/bot.js
@@ -1,20 +1,35 @@
-const { Telegraf } = require('telegraf');
+const {Telegraf} = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const web_link = "https://golden-flan-135f29-6b0ef8.netlify.live/";
+const web_link = 'https://golden-flan-135f29-6b0ef8.netlify.live/';
 
 bot.start((ctx) => {
-  return ctx.reply("Найди нужную практику, узнай расписание и забронируйте место", {
+  return ctx.reply('Найди нужную практику, узнай расписание и забронируйте место', {
     reply_markup: {
-      inline_keyboard: [[{text: "Запустить приложение", web_app: {url: web_link}}]],
+      inline_keyboard: [
+        [
+          {
+            text: 'Запустить приложение',
+            web_app: {url: web_link}
+          }, {
+          text: 'Стать Свами',
+          login_url: {
+            url: 'https://yaswami_bot.t.me',
+            forward_text: 'Стать Свами',
+            bot_username: 'yaswami_bot',
+            request_write_access: true
+          }
+        }
+        ],
+      ],
     },
-  })
+  });
 });
 
 exports.handler = async (event) => {
   try {
     await bot.handleUpdate(JSON.parse(event.body)).then(() => {
-      console.log("Received an update from Telegram!", event.body);
+      console.log('Received an update from Telegram!', event.body);
     });
     return {statusCode: 200};
   } catch (e) {
